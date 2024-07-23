94152598a1352cd8fa5e88922655ee74aaa6aec0
drew walker <drewwalker@ufl.edu>
2024-07-23 14:27:07 -0400

    fixed input for encoded password

--- lab9.py
+++ lab9.py
@@ -1,4 +1,4 @@
-# Takes a string of numbers, and returns a string of the numbers after adding 3. 
+# Takes a string of numbers, and returns a string of the numbers after adding 3.
 # You will implement this function in your repository.
 # Examples:
 # “12345555” will become “45678888” after encoding.
@@ -15,11 +15,10 @@ def encode(password):
 # Takes a string of numbers, and returns a string of the numbers after subtracting 3.
 # You will implement this function in your partner’s repository.
 def decode(password):
-    decoded_chars = []
-    for char in password:
-        decoded_char = str((int(char) + 3) % 10)
-        decoded_chars.append(decoded_char)
-    return ''.join(decoded_chars)
+    decoded_chars = ""
+    for num in password:
+        decoded_chars = decoded_chars + str(int(num) - 3)
+    return decoded_chars
 
 
 def printMenu():
@@ -40,7 +39,7 @@ def main():
         elif option == '2':
 
             try:
-                decoded_password = decode(password)
+                decoded_password = decode(encoded_password)
                 print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
             except:
                 print("Why haven't you put in a password?")
